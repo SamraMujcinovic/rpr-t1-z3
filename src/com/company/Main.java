c5843ca45c5775f3d17eb9f79c6cc97e03c7c79a
samra <smujcinovi1@etf.unsa.ba>
2019-10-14 00:13:25 +0200

    porukaaa123

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //System.out.println("xgnjgmsdfghjkgnfjdklksmghfjkdalg");
+
+        Sat s = new Sat(15,30,45);
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
+
+    }
+}
